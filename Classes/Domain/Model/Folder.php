dda0647a88307794dcafc9c4e2c51f61cdc6ea0a
Nikola Stojiljkovic <nikola.stojiljkovic@essentialdots.com>
2014-12-01 04:27:28 +0100

    [BUGFIX] Fix for the error: cannot use TYPO3\CMS\Core\Resource\File as File
    Change-Id: Iac14223b055a4e0402b47550f92ec8a166e4e135
    Related: #1265

--- Classes/Domain/Model/Folder.php
+++ Classes/Domain/Model/Folder.php
@@ -24,7 +24,6 @@ namespace EssentialDots\ExtbaseFal\Domain\Model;
  *
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
-use TYPO3\CMS\Core\Resource\File;
 
 /**
  * Class File
