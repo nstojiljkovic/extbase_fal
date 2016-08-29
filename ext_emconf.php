fbd3d2f2d52827dbac41ada4963a5168bb0bd4df
Jenkins <jenkins@essentialdots.com>
2016-08-29 09:19:31 +0000

    [RELEASE] Release of extbase_fal 0.2.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,16 +1,7 @@
 <?php
 
-########################################################################
-# Extension Manager/Repository config file for ext "extbase_fal".
-#
-# Auto generated 05-04-2012 19:47
-#
-# Manual updates:
-# Only the data in the array - everything else is removed by next
-# writing. "version" and "dependencies" must not be touched!
-########################################################################
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = array (
 	'title' => 'Extbase FAL domain',
 	'description' => '',
 	'category' => 'plugin',
@@ -29,21 +20,27 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.2.0.dev',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '6.2.0-7.7.0',
-			'extbase' => '',
-			'extbase_hijax' => '',
-			'extbase_domain_decorator' => ''
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-			'ed_tags' => ''
-		),
+	'version' => '0.2.1',
+	'constraints' => 
+	array (
+	  'depends' => 
+	  array (
+	    'typo3' => '6.2.0-7.7.0',
+	    'extbase' => '',
+	    'extbase_hijax' => '',
+	    'extbase_domain_decorator' => '',
+	  ),
+	  'conflicts' => 
+	  array (
+	  ),
+	  'suggests' => 
+	  array (
+	    'ed_tags' => '',
+	  ),
 	),
-	'suggests' => array(
+	'suggests' => 
+	array (
 	),
 );
 
+?>
