88e04a45e1cdf430aea524556ada1dcadaa44b69
Jenkins <jenkins@essentialdots.com>
2016-08-29 18:40:51 +0000

    [TASK] Set extbase_fal version to 0.2.3-dev

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,7 +20,7 @@ $EM_CONF[$_EXTKEY] = array (
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '0.2.2',
+	'version' => '0.2.3-dev',
 	'constraints' => 
 	array (
 	  'depends' => 
